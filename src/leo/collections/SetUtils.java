8c21b3379549fc815ecc5140b50382f0142d616b
Leo Dietrich <leo.felberg@gmail.com>
2017-06-25 19:37:36 +0200

    set utils added

--- src/leo/collections/SetUtils.java
+++ src/leo/collections/SetUtils.java
@@ -8,16 +8,44 @@ public class SetUtils {
      * Returns all elements which are in set1 but not in set2.
      */
     public static <T> Set<T> difference(Set<T> set1, Set<T> set2) {
-        Set result = new HashSet<>();
-
+        Set<T> result = new HashSet<>();
         for (T e : set1) {
-            result.add(e);
-            for (T f : set2) {
-                if (f.equals(e)) {
-                    result.remove(f);
-                }
+            if (!set2.contains(e)) {
+                result.add(e);
             }
         }
         return result;
+    }
+
+    /**
+     * Returns elements from set1 combined with elements from set2.
+     */
+    public static <T> Set<T> union(Set<T> set1, Set<T> set2) {
+        Set<T> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    /**
+     * Returns elements which are contained both in set1 and set2.
+     */
+    public static <T> Set<T> intersection(Set<T> set1, Set<T> set2) {
+        Set<T> result = new HashSet<>();
+        for (T e : set1) {
+            if (set2.contains(e)) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns elements which are contained either in set1 and set2 but not in both.
+     */
+    public static <T> Set<T> symmetricDifference(Set<T> set1, Set<T> set2) {
+        // symmetric difference = union - intersection (- means difference)
+        Set<T> union = union(set1, set2);
+        Set<T> intersection = intersection(set1, set2);
+        return difference(union, intersection);
     }
 }
--- test/leo/collections/SetUtilsTest.java
+++ test/leo/collections/SetUtilsTest.java
@@ -3,7 +3,7 @@ package leo.collections;
 import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
 
-import static leo.collections.SetUtils.difference;
+import static leo.collections.SetUtils.*;
 import static org.junit.Assert.*;
 
 public class SetUtilsTest {
@@ -13,10 +13,39 @@ public class SetUtilsTest {
         assertEquals(ImmutableSet.of(), difference(ImmutableSet.of(), ImmutableSet.of()));
         assertEquals(ImmutableSet.<Integer>of(), difference(ImmutableSet.of(1, 2, 3), ImmutableSet.of(1, 2, 3)));
         assertEquals(ImmutableSet.of(1, 2, 3), difference(ImmutableSet.of(1, 2, 3), ImmutableSet.of()));
-        assertEquals(ImmutableSet.of(1, 3), difference(ImmutableSet.of(1, 2, 3), ImmutableSet.of(2)));
+        assertEquals(ImmutableSet.of(1, 3), difference(ImmutableSet.of(1, 2, 3), ImmutableSet.of(2, 4)));
 
         assertEquals(ImmutableSet.<String>of(), difference(ImmutableSet.of("1", "2", "3"), ImmutableSet.of("1", "2", "3")));
         assertEquals(ImmutableSet.of("1", "2", "3"), difference(ImmutableSet.of("1", "2", "3"), ImmutableSet.of()));
-        assertEquals(ImmutableSet.of("1", "3"), difference(ImmutableSet.of("1", "2", "3"), ImmutableSet.of("2")));
+        assertEquals(ImmutableSet.of("1", "3"), difference(ImmutableSet.of("1", "2", "3"), ImmutableSet.of("2", "4")));
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+        assertEquals(ImmutableSet.of(), union(ImmutableSet.of(), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of(1, 2, 3), union(ImmutableSet.of(1, 2, 3), ImmutableSet.of(1, 2, 3)));
+        assertEquals(ImmutableSet.of(1, 2, 3), union(ImmutableSet.of(1, 2, 3), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of(4, 2, 1, 3), union(ImmutableSet.of(1, 2, 3), ImmutableSet.of(2, 4)));
+
+        assertEquals(ImmutableSet.of("1", "2", "3"), union(ImmutableSet.of("1", "2", "3"), ImmutableSet.of("1", "2", "3")));
+        assertEquals(ImmutableSet.of("1", "2", "3"), union(ImmutableSet.of("1", "2", "3"), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of("2", "1", "4", "3"), union(ImmutableSet.of("1", "2", "3"), ImmutableSet.of("2", "4")));
+    }
+
+
+    @Test
+    public void testIntersection() throws Exception {
+        assertEquals(ImmutableSet.of(), intersection(ImmutableSet.of(), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of(1, 2, 3), intersection(ImmutableSet.of(1, 2, 3), ImmutableSet.of(1, 2, 3)));
+        assertEquals(ImmutableSet.<Integer>of(), intersection(ImmutableSet.of(1, 2, 3), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of(2, 3), intersection(ImmutableSet.of(1, 2, 3), ImmutableSet.of(2, 4, 5, 2, 3)));
+    }
+
+    @Test
+    public void testSymmetricDifference() throws Exception {
+        assertEquals(ImmutableSet.of(), symmetricDifference(ImmutableSet.of(), ImmutableSet.of()));
+        assertEquals(ImmutableSet.<Integer>of(), symmetricDifference(ImmutableSet.of(1, 2, 3), ImmutableSet.of(1, 2, 3)));
+        assertEquals(ImmutableSet.of(1, 2, 3), symmetricDifference(ImmutableSet.of(1, 2, 3), ImmutableSet.of()));
+        assertEquals(ImmutableSet.of(1, 4, 5), symmetricDifference(ImmutableSet.of(1, 2, 3), ImmutableSet.of(2, 4, 5, 2, 3)));
     }
 }
