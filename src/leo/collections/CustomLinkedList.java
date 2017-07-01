2f651720306add1a81dc775b6f77efc6073281ac
Leo Dietrich <leo.felberg@gmail.com>
2017-07-01 10:24:30 +0200

    custom linked list added

---
+++ src/leo/collections/CustomLinkedList.java
@@ -0,0 +1,203 @@
+package leo.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class CustomLinkedList<T> implements List<T> {
+    private class Node<T> {
+        T element;
+        Node<T> next;
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+    private int count = 0;
+
+    @Override
+    public boolean add(T t) {
+        Node<T> node = new Node<>();
+        node.element = t;
+        if (first == null) {
+            first = node;
+            last = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+        count++;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<T> current = first;
+        for (int i = 0; current != null; i++, current = current.next) {
+            if (i == index) {
+                return current.element;
+            }
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node<T> current = first;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[count];
+        Node<T> current = first;
+        for (int i = 0; current != null; i++, current = current.next) {
+            array[i] = current.element;
+        }
+        return array;
+    }
+
+    @Override
+    public void clear() {
+        first = null;
+        last = null;
+        count = 0;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c.isEmpty()) {
+            return false;
+        }
+        for (T e : c) {
+            add(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (first == null) {
+            return false;
+        }
+
+        if (first.element.equals(o)) {
+            if (last == first) {
+                last = first.next;
+            }
+            first = first.next;
+            count--;
+            return true;
+        }
+
+        Node<T> current = first;
+        while (current.next != null) {
+            if (current.next.element.equals(o)) {
+                if (last == current.next) {
+                    last = current;
+                }
+                current.next = current.next.next;
+                count--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
--- test/leo/ListTest.java
+++ test/leo/ListTest.java
@@ -1,34 +1,137 @@
 package leo;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
+import leo.collections.CustomLinkedList;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ListTest {
     @Test
     public void testListProperties() {
-        List<String> list = new ArrayList<>();
+        List<String> list = new CustomLinkedList<>();
         assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+        assertFalse(list.contains("a"));
+        assertFalse(list.contains("b"));
+        assertArrayEquals(new String[]{}, list.toArray());
 
         list.add("a");
         assertEquals(1, list.size());
+        assertFalse(list.isEmpty());
+        assertEquals("a", list.get(0));
+        assertTrue(list.contains("a"));
+        assertFalse(list.contains("b"));
+        assertArrayEquals(new String[]{"a"}, list.toArray());
 
         list.add("a");
         assertEquals(2, list.size());
+        assertFalse(list.isEmpty());
+        assertEquals("a", list.get(0));
+        assertEquals("a", list.get(1));
+        assertTrue(list.contains("a"));
+        assertFalse(list.contains("b"));
+        assertArrayEquals(new String[]{"a", "a"}, list.toArray());
 
         list.add("b");
         assertEquals(3, list.size());
+        assertFalse(list.isEmpty());
+        assertEquals("a", list.get(0));
+        assertEquals("a", list.get(1));
+        assertEquals("b", list.get(2));
+        assertTrue(list.contains("a"));
+        assertTrue(list.contains("b"));
+        assertArrayEquals(new String[]{"a", "a", "b"}, list.toArray());
 
         list.add("a");
         assertEquals(4, list.size());
+        assertFalse(list.isEmpty());
+        assertEquals("a", list.get(0));
+        assertEquals("a", list.get(1));
+        assertEquals("b", list.get(2));
+        assertEquals("a", list.get(3));
+        assertTrue(list.contains("a"));
+        assertTrue(list.contains("b"));
+        assertArrayEquals(new String[]{"a", "a", "b", "a"}, list.toArray());
 
+        /*
         assertEquals(ImmutableList.of("a", "a", "b", "a"), list);
 
         Collections.sort(list);
         assertEquals(ImmutableList.of("a", "a", "a", "b"), list);
+        */
+    }
+
+    @Test
+    public void testClear() {
+        List<String> list = new CustomLinkedList<>();
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+        assertArrayEquals(new String[]{}, list.toArray());
+
+        list.add("a");
+        list.add("b");
+        assertEquals(2, list.size());
+        assertFalse(list.isEmpty());
+        assertArrayEquals(new String[]{"a", "b"}, list.toArray());
+
+        list.clear();
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+        assertArrayEquals(new String[]{}, list.toArray());
+    }
+
+    @Test
+    public void testAddAll() {
+        List<String> list = new CustomLinkedList<>();
+        assertArrayEquals(new String[]{}, list.toArray());
+
+        assertFalse(list.addAll(Collections.<String>emptyList()));
+
+        assertTrue(list.addAll(ImmutableList.of("a", "b", "a", "c")));
+        assertArrayEquals(new String[]{"a", "b", "a", "c"}, list.toArray());
+
+        assertTrue(list.addAll(ImmutableList.of("d", "e", "a", "c")));
+        assertArrayEquals(new String[]{"a", "b", "a", "c", "d", "e", "a", "c"}, list.toArray());
+    }
+
+    @Test
+    public void testContainsAll() {
+        List<String> list = new CustomLinkedList<>();
+        assertTrue(list.containsAll(ImmutableList.of()));
+        assertFalse(list.containsAll(ImmutableList.of("a")));
+
+        list.add("a");
+        list.add("b");
+        assertTrue(list.containsAll(ImmutableList.of("a")));
+        assertTrue(list.containsAll(ImmutableList.of("b")));
+        assertTrue(list.containsAll(ImmutableList.of("a", "b")));
+        assertFalse(list.containsAll(ImmutableList.of("b", "c")));
+    }
+
+    @Test
+    public void testRemove() {
+        List<String> list = new CustomLinkedList<>();
+        assertFalse(list.remove("a"));
+        assertArrayEquals(new String[]{}, list.toArray());
+
+        list.add("a");
+        assertFalse(list.remove("b"));
+        assertTrue(list.remove("a"));
+        assertArrayEquals(new String[]{}, list.toArray());
+
+        list.add("a");
+        list.add("b");
+        assertFalse(list.remove("c"));
+        assertTrue(list.remove("b"));
+        assertArrayEquals(new String[]{"a"}, list.toArray());
+
+        list.add("b");
+        list.add("c");
+        assertTrue(list.remove("b"));
+        assertArrayEquals(new String[]{"a", "c"}, list.toArray());
     }
 }
